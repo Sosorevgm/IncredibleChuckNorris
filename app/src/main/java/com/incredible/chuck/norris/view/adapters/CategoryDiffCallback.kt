ded143d8e8cefdd5194e8ec5aa73312572e2dcf3
Sosorevgm <sosorevgm@gmail.com>
2020-09-16 14:22:36 +0300

    Added CategoryDiffCallback.

---
+++ app/src/main/java/com/incredible/chuck/norris/view/adapters/CategoryDiffCallback.kt
@@ -0,0 +1,17 @@
+package com.incredible.chuck.norris.view.adapters
+
+import androidx.recyclerview.widget.DiffUtil
+
+class CategoryDiffCallback(
+    private val oldCategories: List<String>,
+    private val newCategories: List<String>
+) : DiffUtil.Callback() {
+
+    override fun getOldListSize() = oldCategories.size
+
+    override fun getNewListSize() = newCategories.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) = oldCategories[oldItemPosition] == newCategories[newItemPosition]
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) = oldCategories[oldItemPosition] == newCategories[newItemPosition]
+}
